1eb6dc014d9e95a07e55a528934aa932c44486b8
Lhan <Lhan@169.254.209.86>
2018-06-13 15:23:31 +0800

    我的第二个项目

---
+++ src/main/java/com/g4b/G4bdySpringbootStarter1Application.java
@@ -0,0 +1,12 @@
+package com.g4b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G4bdySpringbootStarter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G4bdySpringbootStarter1Application.class, args);
+	}
+}
